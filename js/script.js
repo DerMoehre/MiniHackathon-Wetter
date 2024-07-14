766c1c4c408c478be0e6b0323ae9652f9af3fe90
Moritz Dahm <94791958+DerMoehre@users.noreply.github.com>
2024-07-14 16:32:07 +0200

    calculating ice melt duration

--- js/script.js
+++ js/script.js
@@ -20,3 +20,27 @@ function generateCurrentDateInnerHMTL(date) {
         <h3>Current Date</h3>
         <p class="text_temperature_date">${date}</p>`;
 }
+
+function heatingTheIce(weightIce, temperatureIce) {
+    return (weightIce * 2.1 *(0-temperatureIce))*1000;
+}
+
+function meltingTheIce(weightIce) {
+    return weightIce * 334000;
+}
+
+function heatFlow(currentTemp, temperatureIce, surfaceIce, heatTransferCoefficient) {
+    return heatTransferCoefficient * surfaceIce * (currentTemp - temperatureIce)
+}
+
+function calculateMeltingTime(currentTemp) {
+    heatTransferCoefficient = 10;
+    surfaceIce = 0.00824;
+    temperatureIce = -13;
+    weightIce = 0.08;
+
+    energyConsumption = heatingTheIce(weightIce, temperatureIce) + meltingTheIce(weightIce);
+    timeNeeded = (energyConsumption/heatFlow(currentTemp, temperatureIce, surfaceIce, heatTransferCoefficient))/60
+
+    return timeNeeded;
+}
